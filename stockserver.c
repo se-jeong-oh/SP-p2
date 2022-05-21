e869c4ec9da598d339c3d45844ecf8db4f2f8e56
undergraduate <cse20181655@cspro8.sogang.ac.kr>
2022-05-20 19:33:52 -0700

    show, buy, sell implemented

--- stockserver.c
+++ stockserver.c
@@ -10,8 +10,147 @@ typedef struct {
     rio_t clientrio[FD_SETSIZE];
 } pool;
 
+typedef struct _Stock{
+    int ID;
+    int left_stock;
+    int price;
+    int readcnt;
+    sem_t mutex;
+} Stock;
+
+typedef struct _Tree{
+    struct _Stock stock;
+    struct _Tree *left;
+    struct _Tree *right;
+} Tree;
+
+Tree *root;
+int show_check = 0;
 int byte_cnt = 0;
 
+void printTree(Tree *root, char *msg) {
+    char buf[MAXLINE];
+    if(root == NULL) return;
+    sprintf(buf, "%d %d %d\n", root->stock.ID, root->stock.left_stock, root->stock.price);
+    strcat(msg, buf);
+    printTree(root->left, msg);
+    printTree(root->right, msg);
+}
+Tree *Insert(Tree *root, int ID, int price, int left_stock) {
+    if(root == NULL) {
+        root = (Tree *)malloc(sizeof(Tree));
+        root->right = root->left = NULL;
+        root->stock.ID = ID;
+        root->stock.left_stock = left_stock;
+        root->stock.price = price;
+    }
+    else {
+        if (ID < root->stock.ID)
+            root->left = Insert(root->left, ID, price, left_stock);
+        else   
+            root->right = Insert(root->right, ID, price, left_stock);
+    }
+    return root;
+}
+Tree *FindStock(int ID) {
+    Tree *curr_node = root;
+    while(1) {
+        if(curr_node->stock.ID == ID)
+            return curr_node;
+        else if(curr_node->stock.ID > ID)
+            curr_node = curr_node->left;
+        else   
+            curr_node = curr_node->right;
+    }
+}
+void saveStock(FILE *fp, Tree *root) {
+    if(root == NULL) return;
+    fprintf(fp, "%d %d %d\n", root->stock.ID, root->stock.left_stock, root->stock.price);
+    saveStock(fp, root->left);
+    saveStock(fp, root->right);
+    return;
+}
+void parseline(char *cmdline, char *argv[]) {
+    int i = 0;
+    char *temp = strtok(cmdline," ");
+    while(temp != NULL) {
+        argv[i] = temp;
+        i++;
+        temp = strtok(NULL, " ");
+    }
+    return;
+}
+void eval(char *argv[], char buf[], int connfd) {
+    int ID, amount;
+    char *msg = (char *)malloc(sizeof(char)*MAXLINE);
+    Tree *curr_node;
+    //printf("argv : %s\n", argv[0]);
+    //printf("here!\n");
+    if(strcmp(argv[0], "show\n") == 0) {
+        printTree(root, msg);
+        strcpy(buf, msg);
+    }
+    else if(strcmp(argv[0], "sell") == 0) {
+        ID = atoi(argv[1]);
+        amount = atoi(argv[2]);
+        curr_node = FindStock(ID);
+        curr_node->stock.left_stock += amount;
+        msg = "[sell] success\n";
+        strcpy(buf, msg);
+    }
+    else if(strcmp(argv[0], "buy") == 0) {
+        ID = atoi(argv[1]);
+        amount = atoi(argv[2]);
+        curr_node = FindStock(ID);
+        if(curr_node->stock.left_stock < amount) {
+            msg = "Not enough left stock\n";
+            strcpy(buf, msg);
+            return;
+        }
+        else {
+            curr_node->stock.left_stock -= amount;
+            msg = "[buy] success\n";
+            strcpy(buf, msg);
+        }
+    }
+    else if(strcmp(argv[0], "exit") == 0) {
+        // Make exit function
+        FILE *fp = fopen("stock.txt", "w");
+        saveStock(fp, root);
+        fclose(fp);
+        Close(connfd);
+    }
+    return;
+}
+void init_stock(void) {
+    FILE *fp;
+    char stockInfo[100];
+    char *ptr;
+    int stockID = 0, stockPrice = 0, stockLeftstock = 0, pos = 0;
+    
+    fp = fopen("stock.txt", "r");
+    while(fgets(stockInfo, 100, fp) != NULL) {
+        ptr = strtok(stockInfo, " ");
+        while (ptr != NULL) {
+            if(pos == 0) stockID = atoi(ptr);
+            else if(pos == 1) stockLeftstock = atoi(ptr);
+            else stockPrice = atoi(ptr);
+            pos++;
+            ptr = strtok(NULL, " ");
+        }
+        root = Insert(root, stockID, stockPrice, stockLeftstock);
+        pos = 0;
+    }
+    fclose(fp);
+}
+void stock_function(char buf[], int connfd) {
+    char *argv[100];
+    char cmdline[100];
+    strcpy(cmdline, buf);
+    parseline(cmdline, argv);
+    eval(argv, buf, connfd);
+    return;
+}
 void init_pool(int listenfd, pool *p) {
     int i;
     p->maxi = -1;
@@ -56,7 +195,9 @@ void check_clients(pool *p) {
                 byte_cnt += n;
                 printf("Server received %d (%d total) bytes on fd %d\n",
                         n, byte_cnt, connfd);
-                for(j=n;j<MAXLINE;j++)
+                printf("%s\n", buf);
+                stock_function(buf, connfd);
+                for(j=strlen(buf);j<MAXLINE;j++)
                     buf[j]='\0';
                 Rio_writen(connfd, buf, MAXLINE);
                 //printf("Waiting! %d\n", connfd);
@@ -80,7 +221,7 @@ int main(int argc, char **argv)
 	    fprintf(stderr, "usage: %s <port>\n", argv[0]);
 	    exit(0);
     }
-
+    init_stock();
     listenfd = Open_listenfd(argv[1]);
     init_pool(listenfd, &pool);
     while (1) {
